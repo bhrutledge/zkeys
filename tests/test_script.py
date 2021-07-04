32822c3e4a2638a6cef84aca97424e4507bfd0d2
Brian Rutledge <brian@bhrutledge.com>
2021-07-04 07:39:10 -0400

    Add types to tests

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,29 +1,32 @@
-# type: ignore
 import re
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytest_console_scripts import ScriptRunner
 
 SCRIPT_NAME = "zkeys"
 SCRIPT_USAGE = f"usage: {SCRIPT_NAME} [-h] [--version]"
 
 
-def test_prints_help(script_runner):
+def test_prints_help(script_runner: ScriptRunner) -> None:
     result = script_runner.run(SCRIPT_NAME, "-h")
     assert result.success
     assert result.stdout.startswith(SCRIPT_USAGE)
 
 
-def test_prints_help_for_invalid_option(script_runner):
+def test_prints_help_for_invalid_option(script_runner: ScriptRunner) -> None:
     result = script_runner.run(SCRIPT_NAME, "-!")
     assert not result.success
     assert result.stderr.startswith(SCRIPT_USAGE)
 
 
-def test_prints_version(script_runner):
+def test_prints_version(script_runner: ScriptRunner) -> None:
     result = script_runner.run(SCRIPT_NAME, "--version")
     assert result.success
     assert re.match(rf"{SCRIPT_NAME} \d+\.\d", result.stdout)
 
 
-def test_prints_keybindings_from_zsh(script_runner):
+def test_prints_keybindings_from_zsh(script_runner: ScriptRunner) -> None:
     result = script_runner.run(SCRIPT_NAME)
     assert result.success
     for line in result.stdout.splitlines():
